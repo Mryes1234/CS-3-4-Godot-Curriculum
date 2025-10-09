9d7ac40eb74c1f4e87af5aef7e97cc2f2e1c5eb4
Aidyn Jackson <ajackson6825@student.pps.net>
2025-10-09 15:38:28 -0700

    summary 4

--- scenes/merchant.gd
+++ scenes/merchant.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_shop_radius_body_entered(_body: Node2D) -> void:
+	if _body is Player:
+		animated_sprite.play("Wave")
+
+func _on_shop_radius_body_exited(_body: Node2D) -> void:
+	if _body is Player:
+		animated_sprite.play("Idle")
