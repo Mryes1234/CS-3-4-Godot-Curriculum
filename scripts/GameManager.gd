84791379b33b3da3b7166c7b6df9b46997524520
Aidyn Jackson <ajackson6825@student.pps.net>
2025-10-20 15:34:39 -0700

    summary 7

---
+++ scripts/GameManager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var shopitems = [
+	preload("res://Health_potion_big.tres"),
+	preload("res://Health_potion_small.tres"),
+	preload("res://Key.tres")
+]
--- scripts/global.gd
+++ scripts/global.gd
@@ -6,4 +6,5 @@ var game_world: GameWorld = null
 
 
 func _ready() -> void:
-	game_world = get_tree().current_scene
+	pass
+#	game_world = get_tree().current_scene
